299f4d0b2f6956c3fc1a8eaf2995b44f2356279b
orangain <orangain@gmail.com>
2021-11-28 16:25:32 +0900

    Configure Auth0Provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const auth0Config = {
+  domain: import.meta.env.VITE_AUTH0_DOMAIN,
+  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+  redirectUri: window.location.origin,
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider {...auth0Config}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
